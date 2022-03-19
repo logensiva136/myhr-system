be4cbd7fd4c110932d0dcfb362f4fb2cf06dd9b1
logensiva136 <logensiva136@gamil.com>
2022-03-19 15:15:55 +0000

    conflict fix

--- app.js
+++ app.js
@@ -11,17 +11,6 @@ require("events").EventEmitter.defaultMaxListeners = 100;
 
 const app = express();
 
-<<<<<<< HEAD
-// app.use(cookieParser());
-app.use(session({
-  secret: process.env.COOKIE_SECRET,
-  saveUninitialized: true,
-  resave: false,
-  // cookie: {
-  //   maxAge: 3600000
-  // }
-}));
-=======
 app.use(cookieParser());
 app.use(
   session({
@@ -33,17 +22,10 @@ app.use(
     },
   })
 );
->>>>>>> d851746ac96196ba8fab6ec59a6c279ee9f56f79
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static(path.join(__dirname, "views", "public")));
-
-<<<<<<< HEAD
-// app.use((req,res)=>console.log("triggered"))
-=======
-// app.use((req, res) => console.log("triggered"));
->>>>>>> d851746ac96196ba8fab6ec59a6c279ee9f56f79
 
 app.use(
   bodyParser.urlencoded({
@@ -57,15 +39,8 @@ app.use("/", userRouter);
 
 app.use((req, res) => {
   res.render("404");
-<<<<<<< HEAD
-})
-
-app.listen(12345, (req, res, next) => {
-  console.log("Listening : http://localhost:12345");
-=======
 });
-const port = process.env.PORT || 12345;
-app.listen(port, "0.0.0.0", (req, res, next) => {
+const port = procces.env.PORT ||  12345;
+app.listen(port, (req, res, next) => {
   console.log("Listening : http://localhost:" + port);
->>>>>>> d851746ac96196ba8fab6ec59a6c279ee9f56f79
 });
